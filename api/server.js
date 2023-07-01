4ef88d54c93fa5a94933ff0f7d5ffee656d2eb2a
Jason Wang <0212jason@gmail.com>
2023-07-01 12:55:16 +1000

    feat: add vercel setting json add cors and export server module

--- api/server.js
+++ api/server.js
@@ -8,10 +8,17 @@ const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 
 const PORT = process.env.JSON_SERVER_PORT;
-
+const uatUrlRegex = new RegExp(".*taotalk.vercel.app/$");
 server.use(middleware);
+server.use(
+  cors({
+    origin: ["http://localhost:3000", uatUrlRegex],
+  })
+);
 
 server.use(router);
 server.listen(PORT, () => {
   console.log(`JSON Server is running at ${PORT}`);
 })
+
+module.exports = server;
